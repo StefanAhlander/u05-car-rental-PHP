a8ee82e2600b93e5ee6c65df7dca77863453f903
stefan Åhlander <cs.ahlander@gmail.com>
2019-12-18 11:47:49 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+  use Main\Core\Router;
+  use Main\Core\Request;
+
+  require_once __DIR__ . "/vendor/autoload.php";
+
+  $loader = new \Twig\Loader\FilesystemLoader(__DIR__ . "/src/Views");
+  $twig = new \Twig\Environment($loader);
+
+  // echo $twig->render('index.html');
+
+  $request = new Request();
+  $router = new Router();
+  echo $router->route($request, $twig);
+?>
