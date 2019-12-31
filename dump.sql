43058e762285bff63bcf9f68b61ac7f33d432fe7
stefan Åhlander <cs.ahlander@gmail.com>
2019-12-31 13:20:33 +0100

    Add database and dummy data

--- dump.sql
+++ dump.sql
@@ -1,71 +1,41 @@
-CREATE SCHEMA IF NOT EXISTS bookstore;
-USE bookstore;
+DROP database IF EXISTS Cars;
+CREATE database Cars;
+USE Cars;
 
-CREATE TABLE book(
-  isbn CHAR(13) NOT NULL,
-  title VARCHAR(255) NOT NULL,
-  author VARCHAR(255) NOT NULL,
-  stock SMALLINT UNSIGNED NOT NULL DEFAULT 0,
-  price FLOAT UNSIGNED
+CREATE TABLE customers(
+  personnumber BIGINT PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  address VARCHAR(255) NOT NULL,
+  postaladress VARCHAR(255) NOT NULL,
+  phonenumber INT NOT NULL
 ) ENGINE=InnoDb;
 
-CREATE TABLE customer(
-  id INT UNSIGNED NOT NULL,
-  firstname VARCHAR(255) NOT NULL,
-  surname VARCHAR(255) NOT NULL,
-  email VARCHAR(255) NOT NULL,
-  type ENUM('basic', 'premium')
+CREATE TABLE cars(
+  registration CHAR(6) PRIMARY KEY,
+  make VARCHAR(100) NOT NULL,
+  color VARCHAR(20) NOT NULL,
+  year INT NOT NULL,
+  price INT NOT NULL,
+  checkedoutby INT,
+  checkedouttime DATETIME
 ) ENGINE=InnoDb;
 
-ALTER TABLE book
-  ADD id INT UNSIGNED NOT NULL AUTO_INCREMENT 
-  PRIMARY KEY FIRST;
-
-ALTER TABLE customer
-  MODIFY id INT UNSIGNED NOT NULL
-  AUTO_INCREMENT PRIMARY KEY;
-
-CREATE TABLE borrowed_books(
-  book_id INT UNSIGNED NOT NULL,
-  customer_id INT UNSIGNED NOT NULL,
-  start DATETIME NOT NULL,
-  end DATETIME DEFAULT NULL,
-  FOREIGN KEY (book_id) REFERENCES book(id),
-  FOREIGN KEY (customer_id) REFERENCES customer(id)
-) ENGINE=InnoDb;
-
-CREATE TABLE sale(
+CREATE TABLE rentals(
   id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  customer_id INT UNSIGNED NOT NULL,
-  date DATETIME NOT NULL,
-  FOREIGN KEY (customer_id) REFERENCES customer(id)
+  registration CHAR(6) NOT NULL,
+  personnumber BIGINT NOT NULL,
+  checkouttime DATETIME NOT NULL,
+  checkintime DATETIME NOT NULL,
+  days INT NOT NULL,
+  cost INT NOT NULL,
+  FOREIGN KEY (registration) REFERENCES cars(registration),
+  FOREIGN KEY (personnumber) REFERENCES customers(personnumber)
 ) ENGINE=InnoDb;
 
-CREATE TABLE sale_book(
-  sale_id INT UNSIGNED NOT NULL,
-  book_id INT UNSIGNED NOT NULL,
-  amount SMALLINT UNSIGNED NOT NULL DEFAULT 1,
-  price FLOAT NOT NULL,
-  FOREIGN KEY (sale_id) REFERENCES sale(id),
-  FOREIGN KEY (book_id) REFERENCES book(id)
-) ENGINE=InnoDb;
+INSERT INTO customers (personnumber, name, address, postaladress, phonenumber) VALUES
+  (7302271452, "Stefan Åhlander", "Lapplandsresan 25 B", "75755 Uppsala", 0704979766),
+  (8007271482, "Maria Erlandsson", "Lapplandsresan 25 B", "75755 Uppsala", 0725100580);
 
-ALTER TABLE book ADD UNIQUE KEY (isbn);
-ALTER TABLE customer ADD UNIQUE KEY (email);
-ALTER TABLE book ADD INDEX (title);
-
-INSERT INTO customer (firstname, surname, email, type)
-  VALUES ("Han", "Solo", "han@tatooine.com", "premium");
-INSERT INTO customer (firstname, surname, email, type)
-  VALUES ("James", "Kirk", "enter@prise", "basic");
-
-INSERT INTO book (isbn,title,author,stock,price) VALUES
-  ("9780882339726","1984","George Orwell",12,7.50),
-  ("9789724621081","1Q84","Haruki Murakami",9,9.75),
-  ("9780736692427","Animal Farm","George Orwell",8,3.50),
-  ("9780307350169","Dracula","Bram Stoker",30,10.15),
-  ("9780753179246","19 minutes","Jodi Picoult",0,10);
-
-INSERT INTO book (isbn,title,author,price) VALUES
-  ("9781416500360", "Odyssey", "Homer", 4.23);
-
+INSERT INTO cars (registration, make, color, year, price) VALUES
+  ("LSW364", "Toyota", "Gray", 2012, 500),
+  ("DOL990", "Toyota", "Red", 2011, 200);
